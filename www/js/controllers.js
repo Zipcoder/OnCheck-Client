49964adb1aa7a9ed5aa2589b6752228fc9fb35b2
Wesley Traute <wesleytraute@Zipcoder20s-MacBook-Pro.local>
2016-07-01 08:15:26 -0400

    Added evidence upload to tips

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -23,10 +23,44 @@ angular.module('starter.controllers', [])
   }
   init();
 })
-.controller('tipCtrl', function($scope, $http) {
-   submitTip = function(){
-     var url = "/tips";
 
-     $http.post
-   }
+.controller('tipCtrl', function($scope, $http, fileUpload) {
+
+  var evidence;
+
+  $scope.tip = {
+    message: ''
+  };
+
+  $scope.clear = function() {
+    $scope.tip.message = '';
+    $scope.myFile = '';
+  };
+
+  $scope.uploadFile = function() {
+    var file = $scope.myFile;
+
+    console.log('file is ');
+    console.dir(file);
+
+    var uploadUrl = "file:///users/wesleytraute/Downloads/";
+    evidence = uploadUrl+file.name;
+
+    fileUpload.uploadFileToUrl(file, uploadUrl);
+  };
+
+  $scope.submitTip = function() {
+    var url = "localhost:8080/tips";
+    var tip =
+    {
+     "message" : $scope.tip.message,
+     "evidence" : evidence,
+     "user_id" : "placeholder",
+     "location_id" : "placeholder"
+    };
+
+    $scope.uploadFile();
+    $http.post(url, tip);
+    $scope.clear();
+  };
 })
