e6a560148b955775dea71699783b3a999004aed9
Wesley Traute <wesleytraute@Zipcoder20s-MacBook-Pro.local>
2016-07-05 11:17:20 -0400

    Changed ui, image upload fixed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,43 +26,36 @@ angular.module('starter.controllers', [])
 
 .controller('tipCtrl', function($scope, $http, fileUpload) {
 
-  var evidence;
+  $scope.evidence;
 
-  $scope.tip = {
-    message: ''
-  };
+  //Displays Image after Uploaded
+  document.getElementById("picture").onchange = function () {
+    var reader = new FileReader();
 
-  $scope.clear = function() {
-    $scope.tip.message = '';
-    $scope.myFile = '';
+    reader.onload = function (e) {
+
+        document.getElementById("thumbnail").src = e.target.result;
+
+        var fileNameArray = document.getElementById('picture').value.split("\\");
+        $scope.evidence = fileNameArray[fileNameArray.length-1];
+    };
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
   };
 
   $scope.uploadFile = function() {
-    var file = $scope.myFile;
+    var file =  document.getElementById('picture').files[0];
 
     console.log('file is ');
     console.dir(file);
 
     var uploadUrl = "http://localhost:8080/evidence/";
-    console.log(uploadUrl+String(file));
 
     fileUpload.uploadFileToUrl(file, uploadUrl);
   };
 
-  $scope.submitTip = function() {
-    var url = "http://localhost:8080/tips";
-    var tip =
-    {
-     "message" : $scope.tip.message,
-     "evidence" : evidence,
-     "user_id" : "placeholder",
-     "location_id" : "placeholder"
-    };
-
-    console.log(evidence);
-
-    $scope.uploadFile();
-    $http.post(url, tip);
-    $scope.clear();
+  $scope.clear = function() {
+    document.getElementById("thumbnail").src = "";
   };
+
 })
