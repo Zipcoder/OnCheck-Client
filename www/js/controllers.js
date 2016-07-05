31bfb64d4534b8455540bec9436172411d612c96
Wesley Traute <wesleytraute@Zipcoder20s-MacBook-Pro.local>
2016-07-05 12:56:05 -0400

    Commit before creating new branch

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -26,6 +26,47 @@ angular.module('starter.controllers', [])
 
 .controller('tipCtrl', function($scope, $http, fileUpload) {
 
+  /*var evidence;
+
+  $scope.tip = {
+    message: ''
+  };
+
+  $scope.clear = function() {
+    $scope.tip.message = '';
+    $scope.myFile = '';
+  };
+
+  $scope.uploadFile = function() {
+    var file = $scope.myFile;
+
+    console.log('file is ');
+    console.dir(file);
+
+    var uploadUrl = "http://localhost:8080/evidence/";
+    console.log(uploadUrl+String(file));
+
+    fileUpload.uploadFileToUrl(file, uploadUrl);
+  };
+
+  $scope.submitTip = function() {
+    var url = "http://localhost:8080/tips";
+    var tip =
+    {
+     "message" : $scope.tip.message,
+     "evidence" : evidence,
+     "user_id" : "placeholder",
+     "location_id" : "placeholder"
+    };
+
+    console.log(evidence);
+
+    $scope.uploadFile();
+    $http.post(url, tip);
+    $scope.clear();
+  };*/
+
+
   $scope.evidence;
 
   //Displays Image after Uploaded
